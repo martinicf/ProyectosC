5c37bccee68cef2977f188375a91f8fb8377d2b8
Martin Correa Forte <tinch016@live.com.ar>
2022-04-17 22:26:44 -0300

    Commit a new file

---
+++ lab01/some.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+void swap (int a, int b) {
+    int tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+
+int main (void) {
+
+    //Just think you're swapping something here
+    return EXIT_SUCCESS;
+}
